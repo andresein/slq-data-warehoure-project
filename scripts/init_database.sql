f2c2228a8d289ac485e0cbc1f1bebed94d31f6ee
Andres Coral <109749148+andresein@users.noreply.github.com>
2026-02-03 10:38:33 -0500

    Create DataWarehouse and schemas
    This script creates a new Datawarehouse database and three schemas: bronze, silver, and gold. It also includes a warning about dropping the existing database.

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,38 @@
+/*
+==================================
+Create Database and Schemas
+==================================
+Purpose:
+	This script create a new Datawarehouse and 3 schemas.
 
+WARNING:
+	Will drop the entire 'Datawarehouse. Proceed with caution
+
+*/
+
+
+USE master;
+GO
+
+-- Drop and recreate the 'DataWarehouse' database
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name='DataWarehouse')
+BEGIN
+	ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK INMEDIATE;
+	DROP DATABASE DataWarehouse;
+END;
+GO
+
+--Create the 'DataWarehouse' database
+CREATE DATABASE DataWarehouse;
+GO
+
+Use DataWarehouse;
+GO
+
+-- Create Schemas
+CREATE SCHEMA bronze;
+GO
+CREATE SCHEMA silver;
+GO
+CREATE SCHEMA gold;
+GO
